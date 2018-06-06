4a457f834619eb6eaeb29f61cff90621d510a5c1
Scherlock90 <31919808+Scherlock90@users.noreply.github.com>
2018-06-06 10:26:33 +0200

    Upload better version

--- Anonimowe_klasy.java
+++ Anonimowe_klasy.java
@@ -2,31 +2,35 @@ package zagniezdzenieKlas;
 
 import java.net.NetworkInterface;
 
-public class Anonimowe_klasy {
+public class Anonimowe_klasy 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
 
-		zachowaniePrzycisku zP = new zachowaniePrzycisku() {
+		zachowaniePrzycisku zP = new zachowaniePrzycisku() 
+		{
 
 			@Override
-			public void działaniePrzycisku() {
+			public void działaniePrzycisku() 
+			{
 				// TODO Auto-generated method stub	
 				System.out.println("Jestem z klasy anonimowej przyciskiem zP");
 			}
 		};
 		
-		zachowaniePrzycisku m = new zachowaniePrzycisku() {		//jest to klasa anonimowa - bez nazwy, do której trzeba będzie zimplementować metodę z intefejsu oraz na końcu po } klamerce dać znak ; by działała istotne.
+		zachowaniePrzycisku m = new zachowaniePrzycisku() 
+		{		
 
 			@Override
-			public void działaniePrzycisku() {		//tutaj możemy ustalić indywidualne działanie przycisku m - zmiennej ustalonej powyżej
+			public void działaniePrzycisku() 
+			{		
 				// TODO Auto-generated method stub
 				System.out.println("M jak malina, a tak naprawdę mogę odpowiadać za przycisk M w klasie anonimowej");
 			}		
 		};
-		
-		// aktywacje przycisku i zachowania do niego przyporządkowanego
-		
+			
 		zP.działaniePrzycisku();
 		m.działaniePrzycisku();
 	}
@@ -35,10 +39,10 @@ public class Anonimowe_klasy {
 
 interface zachowaniePrzycisku 
 {
-	void działaniePrzycisku();			// będzie implementowana przez każdy przycisk nowy
+	void działaniePrzycisku();			
 }
 
-class przycisk		//tworzenie pojedynczych klass do przycisków o podobnym działaniu, itp. może być bezsensowne
+class przycisk		
 {
 	void akcja()
 	{
