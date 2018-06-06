37cc23cbf76b8d6b78173cf1966f2b7488b0c897
Scherlock90 <31919808+Scherlock90@users.noreply.github.com>
2018-06-06 11:03:31 +0200

    Upload better version

--- Tablice_Arrays.java
+++ Tablice_Arrays.java
@@ -5,8 +5,8 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-public class Tablice_Arrays implements Comparable {
-
+public class Tablice_Arrays implements Comparable 
+{
 	double liczba;
 	private double Tablice_Arrays;
 	
@@ -24,43 +24,36 @@ public class Tablice_Arrays implements Comparable {
 		return liczba;
 	}
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
 		
-		Tablice_Arrays[] tab = new Tablice_Arrays[4];		//do sorotowania odwrotnego tablicy zwykłej niezbędne było umiejscowienie jej w tablicy z konstruktora
+		Tablice_Arrays[] tab = new Tablice_Arrays[4];		
 		
-		//double[] tab = new double[4];
-		
-		tab[0] = new Tablice_Arrays(-19);		//dzięki konstruktorowi mogłem zastosować sortowanie odwrotne
+		tab[0] = new Tablice_Arrays(-19);	
 		tab[1] = new Tablice_Arrays(-9);
 		tab[2] = new Tablice_Arrays(9);
 		tab[3] = new Tablice_Arrays(99);
 		
 		System.out.println("Sortowanie od najmniejszej do największej");
-		Arrays.sort(tab);		// sortuje od najmniejszej do największej
+		Arrays.sort(tab);	
 		
-		for (Tablice_Arrays sortowanko: tab)		// w tej pętli musi znajdować się odpowiedni typ zmiennej String (gdy są nazwy), int gdy tablica jest zbiorem liczb itd.
+		for (Tablice_Arrays sortowanko: tab)		
 		{
-			 System.out.println(sortowanko.getLiczba());		//odniesienie do nowej zmiennej aby wyświetliwło zawartość tablicy	
+			 System.out.println(sortowanko.getLiczba());			
 		}
 		System.out.println("Sortowanie od największej do najmniejszej");
-		Arrays.sort(tab, Collections.reverseOrder());		// sortuje od najmniejszej do największej
+		Arrays.sort(tab, Collections.reverseOrder());		
 		
-		for (Tablice_Arrays sortowanko: tab)		// w tej pętli musi znajdować się odpowiedni typ zmiennej String (gdy są nazwy), int gdy tablica jest zbiorem liczb itd.
+		for (Tablice_Arrays sortowanko: tab)		
 		{
-			 System.out.println(sortowanko.getLiczba());		//odniesienie do nowej zmiennej aby wyświetliwło zawartość tablicy	
-		}			//żeby nie zobaczyć hashcode'u należy użyć gettera do którego odnosimy się zmienną.
-		
-		System.out.println("Porównywanie, tzn. CompareTo: " + tab[0].compareTo(tab[3])); 	//porównywanie za pomocą compareTo, było możliwe po ustanowieniu odpowiedniej instacji w tablicy z konstruktorem
-																							//... i ustawienie w metodzie compareTo object
+			 System.out.println(sortowanko.getLiczba());		
+		}					
+		System.out.println("Porównywanie, tzn. CompareTo: " + tab[0].compareTo(tab[3])); 	
 	}
-
 	
 	@Override
-	public int compareTo(Object o) {
-		// TODO Auto-generated method stub
-		
+	public int compareTo(Object o) 
+	{
 		Tablice_Arrays przeslany =  (Tablice_Arrays)o;
 		
 		if (this.getLiczba() > przeslany.getLiczba())
@@ -68,7 +61,7 @@ public class Tablice_Arrays implements Comparable {
 			return 1;
 		}
 		
-		if (this.getLiczba() < przeslany.getLiczba())	//getter ułatwia ustawienie
+		if (this.getLiczba() < przeslany.getLiczba())	
 		{
 			return -1;
 		}
